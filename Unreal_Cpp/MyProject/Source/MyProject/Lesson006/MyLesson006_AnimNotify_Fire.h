940ed7e168453c674a104cd75df9e5f41f0ca985
SimpleTalkCpp <simpletalkcpp@gmail.com>
2022-02-08 16:12:25 +0100

    unreal C++ - Lesson006.umap

--- Unreal_Cpp/MyProject/Source/MyProject/Lesson006/MyLesson006_AnimNotify_Fire.h
+++ Unreal_Cpp/MyProject/Source/MyProject/Lesson006/MyLesson006_AnimNotify_Fire.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "../Common/MyCommon.h"
 #include <Animation/AnimNotifies/AnimNotify.h>
+#include <Animation/AnimNotifies/AnimNotifyState.h>
+
 #include "MyLesson006_AnimNotify_Fire.generated.h"
 
 UCLASS()
@@ -11,4 +14,22 @@ public:
 	int testInt = 1;
 
 	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+};
+
+UCLASS()
+class UMyLesson006_AnimNotifyState_Example : public UAnimNotifyState {
+	GENERATED_BODY()
+public:
+
+	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration) { 
+		MY_LOG("AnimNotifyState_Example Begin"); 
+	}
+	
+	virtual void NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime) {
+//		MY_LOG("AnimNotifyState_Example Tick"); 
+	}
+	
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation) {
+		MY_LOG("AnimNotifyState_Example End");
+	}
 };
